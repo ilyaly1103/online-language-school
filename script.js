651a275a879aa4a566af9cafedde5969be57190e
ilyaly1103 <ilyalysenko1103@gmail.com>
2024-10-03 00:59:39 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var toggleButtons = document.querySelectorAll(".toggle-button");
-
+    let toggleButtons = document.querySelectorAll(".toggle-button");
+    let punkt = document.querySelectorAll("#punkt");
+    const card = document.querySelectorAll("#card")
+    
     toggleButtons.forEach(function(button) {
         button.addEventListener("click", function() {
-            var targetId = button.getAttribute("data-target");
-            var targetElement = document.querySelector(targetId);
+            let targetId = button.getAttribute("data-target");
+            let targetElement = document.querySelector(targetId);
             
             if (targetElement.classList.contains("show")) {
                 targetElement.classList.remove("show");
@@ -12,8 +14,32 @@ document.addEventListener("DOMContentLoaded", function() {
             } else {
                 targetElement.classList.add("show");
                 button.classList.remove("collapsed");
+                $(targetElement).hide().fadeIn(600);
             }
             
         });
     });
+
+    punkt[0].classList.add("punkt-color");
+
+    punkt.forEach(function(btn) {
+        btn.addEventListener("click", function() {
+            punkt.forEach(function(button) {
+                button.classList.remove("punkt-color");
+            });
+            card.forEach(function(button){
+                button.classList.remove("card-active");
+            });
+            btn.classList.add("punkt-color");
+
+            const index = this.getAttribute("data-index");
+            const cardActive = document.querySelectorAll("#card")[index];
+
+            $(cardActive).hide().fadeIn(600);
+            cardActive.classList.add("card-active");
+            console.log('Нажата точка с индексом:', index);
+
+        });
+    });
 });
+
